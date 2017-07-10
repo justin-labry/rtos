66b7c625bfa6e2d474434292b50c417aee29bd54
makerj <ohenwkgdj@gmail.com>
2017-07-10 13:43:48 +0900

    Fix build error

--- lib/ext/src/types.c
+++ lib/ext/src/types.c
@@ -9,7 +9,7 @@
 bool is_uint8(const char* val) {
 	char* end = NULL;
 	errno = 0;
-	long int v = strtoul(val, &end, 0);
+	long int v = __strtoul(val, &end, 0);
 
 	if(end == NULL || *end != '\0') return false;
 	if(v > UINT8_MAX) return false;
@@ -19,13 +19,13 @@ bool is_uint8(const char* val) {
 }
 
 uint8_t parse_uint8(const char* val) {
-	return strtoul(val, NULL, 0);
+	return __strtoul(val, NULL, 0);
 }
 
 bool is_uint16(const char* val) {
 	char* end = NULL;
 	errno = 0;
-	unsigned long int v = strtoul(val, &end, 0);
+	unsigned long int v = __strtoul(val, &end, 0);
 
 	if(end == NULL || *end != '\0') return false;
 	if(v > UINT16_MAX) return false;
@@ -35,7 +35,7 @@ bool is_uint16(const char* val) {
 }
 
 uint16_t parse_uint16(const char* val) {
-	return strtoul(val, NULL, 0);
+	return __strtoul(val, NULL, 0);
 }
 
 bool is_uint32(const char* val) {
@@ -49,7 +49,7 @@ bool is_uint32(const char* val) {
 }
 
 uint32_t parse_uint32(const char* val) {
-	return strtoul(val, NULL, 0);
+	return __strtoul(val, NULL, 0);
 }
 
 bool is_uint64(const char* val) {
@@ -58,10 +58,10 @@ bool is_uint64(const char* val) {
 
 	if(end == NULL || *end != '\0') return false;
 	if(errno == ERANGE) return false;
-	
+
 	return true;
 }
 
 uint64_t parse_uint64(const char* val) {
-	return strtoull(val, NULL, 0);
+	return __strtoull(val, NULL, 0);
 }
