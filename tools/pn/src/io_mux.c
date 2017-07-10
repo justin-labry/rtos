284632924e798a45a7ae3512ab73bdcdca2ea3c9
sunghokim <sungho@gurum.cc>
2017-07-10 14:39:21 +0900

    IO_Mux is fixed
    * Checking table is null is adedd
    * Write event is called eventy event time

--- tools/pn/src/io_mux.c
+++ tools/pn/src/io_mux.c
@@ -15,64 +15,71 @@ static struct timeval tv; //Busy Poll
 static Map* io_mux_table;
 
 bool io_mux_poll(void* context) {
-    fd_set read_fds, write_fds;
-    read_fds = write_fds = fds;
+	fd_set read_fds;
+	read_fds = fds;
 
-    int retval = select(max_fd + 1, &read_fds, &write_fds, NULL, &tv);
+	int retval = select(max_fd + 1, &read_fds, NULL, NULL, &tv);
 
-    if(retval == -1) {
-        perror("IO Mux Error\n");
-    } else if(retval) {
-        MapIterator iter;
-        map_iterator_init(&iter, io_mux_table);
-        while(map_iterator_has_next(&iter)) {
-            MapEntry* entry = map_iterator_next(&iter);
-            IOMultiplexer* io_mux = entry->data;
+	if(retval == -1) {
+		perror("IO Mux Error\n");
+	} else if(retval) {
+		MapIterator iter;
+		map_iterator_init(&iter, io_mux_table);
+		while(map_iterator_has_next(&iter)) {
+			MapEntry* entry = map_iterator_next(&iter);
+			IOMultiplexer* io_mux = entry->data;
 
-            //Read Handle
-            if(FD_ISSET(io_mux->fd, &read_fds)) {
-                if(io_mux->read_handler && io_mux->read_handler(io_mux->fd, io_mux->context) < 0) {
-                    perror("IO Mux Read Error\n");
-                }
-            }
+			//Read Handle
+			if(FD_ISSET(io_mux->fd, &read_fds)) {
+				if(io_mux->read_handler && io_mux->read_handler(io_mux->fd, io_mux->context) < 0) {
+					perror("IO Mux Read Error\n");
+				}
+			}
+		}
+	}
 
-            //Write Handle
-            if(FD_ISSET(io_mux->fd, &write_fds)) {
-                if(io_mux->write_event && io_mux->write_event(io_mux->fd, io_mux->context) < 0) {
-                    perror("IO Mux Read Error\n");
-                }
-            }
+	//Write Event
+	MapIterator iter;
+	map_iterator_init(&iter, io_mux_table);
+	while(map_iterator_has_next(&iter)) {
+		MapEntry* entry = map_iterator_next(&iter);
+		IOMultiplexer* io_mux = entry->data;
 
-            //TODO: Error Handle
-        }
-    }
+		if(io_mux->write_event && io_mux->write_event(io_mux->fd, io_mux->context) < 0) {
+			perror("IO Mux Read Error\n");
+		}
+	}
 
-    return true;
+	return true;
 }
 
 bool io_mux_init() {
-    FD_ZERO(&fds);
-    io_mux_table = map_create(16, NULL, NULL, NULL);
-    if(!io_mux_table) return false;
+	FD_ZERO(&fds);
+	io_mux_table = map_create(32, NULL, NULL, NULL);
+	if(!io_mux_table) return false;
 
-    event_busy_add(io_mux_poll, NULL);
-    return true;
+	event_busy_add(io_mux_poll, NULL);
+	return true;
 }
 
 bool io_mux_add(IOMultiplexer* io_mux, uint64_t key) {
-    if(!io_mux) return false;
+	if(!io_mux) return false;
 
-    if(!map_put(io_mux_table, (void*)key, io_mux)) return false;
+	if(!io_mux_table) return false;
 
-    FD_SET(io_mux->fd, &fds);
-    if(max_fd < io_mux->fd) max_fd = io_mux->fd;
+	if(!map_put(io_mux_table, (void*)key, io_mux)) return false;
 
-    return true;
+	FD_SET(io_mux->fd, &fds);
+	max_fd = max_fd < io_mux->fd ? io_mux->fd : max_fd;
+
+	return true;
 }
 
 IOMultiplexer* io_mux_remove(uint64_t key) {
-    IOMultiplexer* io_mux = map_remove(io_mux_table, (void*)key);
-    FD_CLR(io_mux->fd, &fds);
+	if(!io_mux_table) return NULL;
 
-    return io_mux;
+	IOMultiplexer* io_mux = map_remove(io_mux_table, (void*)key);
+	FD_CLR(io_mux->fd, &fds);
+
+	return io_mux;
 }
