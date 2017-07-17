d2390958db4356152c0ab41c4b23abf00645d660
makerj <ohenwkgdj@gmail.com>
2017-07-17 18:05:34 +0900

    More strict kernel module checking

--- tools/pn/start.sh
+++ tools/pn/start.sh
@@ -4,6 +4,12 @@ set -e
 BOOT_PARAM=`cat packetngin-boot.param`
 echo $BOOT_PARAM
 
+MSR_TOOLS_EXISTS=$(dpkg --get-selections| grep msr-tools | wc -l)
+if (( $MSR_TOOLS_EXISTS < 1 )); then
+	echo "msr-tools is not found. (perhaps you forgot to install this package?)"
+	exit 1
+fi
+
 sudo insmod ./drivers/dispatcher.ko
 sudo modprobe msr
 sudo ./pnd $BOOT_PARAM
